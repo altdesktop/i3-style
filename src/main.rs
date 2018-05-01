9fb7a8274f3cc16e81ef188887157b2e82850c3e
Tony Crisci <tony@dubstepdish.com>
2018-05-01 18:53:45 -0400

    basic command line parser

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,55 @@ use std::fs::File;
 extern crate yaml_rust;
 use yaml_rust::{YamlLoader};
 
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 mod theme;
 mod writer;
 
+fn main() {
+    let matches = App::new("i3-style")
+        .version("1.0")
+        .about("Make your i3 config a bit more stylish")
+        .arg(Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .value_name("file")
+            .help("The config file the theme should be applied to. Defaults to the default i3 location.")
+            .takes_value(true)
+            )
+        .arg(Arg::with_name("output")
+             .short("o")
+             .long("output")
+             .value_name("file")
+             .help("Apply the theme, attempt to validate the result, and write it to <file>")
+             .takes_value(true)
+            )
+        .arg(Arg::with_name("save")
+             .short("s")
+             .long("save")
+             .help("Set the output file to the path of the input file")
+            )
+        .arg(Arg::with_name("reload")
+             .short("r")
+             .long("reload")
+             .help("Apply the theme by reloading the config")
+            )
+        .arg(Arg::with_name("list-all")
+             .short("l")
+             .long("list-all")
+             .help("Print a list of all available themes")
+            )
+        .arg(Arg::with_name("to-theme")
+             .short("t")
+             .long("to-theme")
+             .value_name("file")
+             .help("Prints an i3-style theme based on the given config suitable for sharing with others")
+             .takes_value(true)
+            ).get_matches();
+}
+
+/*
 fn main() {
     let path = "./test-theme.yaml".to_string();
     let mut f = File::open(path).expect("file does not exist");
@@ -25,3 +71,4 @@ fn main() {
 
     writer::write_config(f, Option::None, theme);
 }
+*/
